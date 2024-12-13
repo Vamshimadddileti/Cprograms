c0948361215b21a7c8fc3c842a66795060ebf9ee
vamshisangala <svamshi2105@gmail.com>
2024-12-13 21:23:12 +0530

    CE-24242: Adding first program

---
+++ vam1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter n value:");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("n is even number");
+    }
+    else
+    {
+        printf("n is odd number");
+    }
+    return 0;
+}
